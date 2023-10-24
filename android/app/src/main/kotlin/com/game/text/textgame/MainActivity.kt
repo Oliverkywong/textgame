ceaace1972906a45ccbd13799b623eb33331fb6e
Oliverkywong <tom23400@gmail.com>
2023-10-24 20:57:37 +0800

    first commit

---
+++ android/app/src/main/kotlin/com/game/text/textgame/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.game.text.textgame
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+// import 'package:textgame/Data.dart';
+// import 'package:xml/xml.dart' as xml;
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Text Game',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Text Game'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  // Future<List<Con>> getConFromXML(BuildContext context) async {
+  //   String datalist =
+  //   await DefaultAssetBundle.of(context).loadString("assets/data/data.xml");
+  //   var raw = xml.XmlDocument.parse(datalist);
+  //   var elements = raw.findAllElements('contact');
+  //   return elements.map((element) {
+  //     return Con(element.findElements("name").first.text,
+  //         int.parse(element.findElements("age").first.text));
+  //   }).toList();
+  // }
+
+  void _loadData() async {
+    final temporaryList = [];
+
+    // Code for parsing XML data.
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: <Widget>[
+            const Text(
+              'GAME',
+              style: TextStyle(fontSize: 50, fontWeight: FontWeight.bold),
+            ),
+            const SizedBox(height: 30),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                  textStyle: const TextStyle(fontSize: 20)),
+              onPressed: () {},
+              child: const Text('Start'),
+            ),
+          ],
+        ),
+      ),
+    );
+    //   body: Center(
+    //     child: FutureBuilder(
+    //       future: getConFromXML(context),
+    //       builder: (context, data){
+    //         if(data.hasData){
+    //           List<Con>? contacts = data.data;
+    //           return ListView.builder(
+    //             itemCount: contacts!.length,
+    //               itemBuilder: (context, index){
+    //               return ListTile(
+    //                 title: Text(contacts[index].name),
+    //                 subtitle: Text(contacts[index].age.toString()),
+    //               );
+    //           });
+    //         }else{
+    //           return Center(child: CircularProgressIndicator(),);
+    //         }
+    //       },
+    //     ),
+    //   ),
+    //   floatingActionButton: FloatingActionButton(
+    //     onPressed: _incrementCounter,
+    //     tooltip: 'Increment',
+    //     child: const Icon(Icons.add),
+    //   ),
+    // );
+  }
+}
