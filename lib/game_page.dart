0547a0013148e4b9daa74e04857fae1ecc557351
oliver.wong <oliver.wong@TECHNET-ASIA.COM>
2023-10-26 17:55:03 +0800

    fight

--- lib/game_page.dart
+++ lib/game_page.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
+import 'dart:async';
 
 class GamePage extends StatefulWidget {
   const GamePage({super.key});
 
   @override
-  State<GamePage> createState() =>
-      GamePageState();
+  State<GamePage> createState() => GamePageState();
 }
 
-class GamePageState
-    extends State<GamePage> {
-  int _selectedIndex = 0;
+class GamePageState extends State<GamePage> {
+  int _selectedIndex = 1;
+  bool fight = false;
   final List<Widget> _widgetOptions = <Widget>[
-    HomeScreen(),
+    const HomeScreen(),
     const Game(),
-    ProfileScreen(),
+    const ProfileScreen()
   ];
 
   void _onItemTapped(int index) {
@@ -53,6 +53,8 @@ class GamePageState
 }
 
 class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Center(
@@ -60,7 +62,6 @@ class HomeScreen extends StatelessWidget {
     );
   }
 }
-
 
 class Game extends StatefulWidget {
   const Game({super.key});
@@ -70,56 +71,115 @@ class Game extends StatefulWidget {
   }
 }
 
+class GameState extends State<Game> {
+  @override
+  Widget build(BuildContext context) => Scaffold(
+          body: Center(
+              child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text('welcome 1-1 round'),
+          Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('left'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('forward'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('right'),
+              ),
+            ],
+          )
+        ],
+      )));
+}
+
+class Fight extends StatefulWidget {
+  const Fight({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return FightState();
+  }
+}
+
 class Monster {
   String name = 'Monster';
-  int hp = 100;
-  int attack = 8;
+  int lv = 21;
+  late double hp = 100 * (1 + (lv - 1) / 10);
+  late double attack = 8 * (1 + (lv - 1) / 10);
 }
 
 class Player {
   String name = 'Player';
-  int hp = 100;
-  int attack = 10;
+  int lv = 21;
+  late double hp = 100 * (1 + (lv - 1) / 10);
+  late double attack = 10 * (1 + (lv - 1) / 10);
 }
 
-class GameState extends State<Game> {
+class FightState extends State<Fight> {
   var monster = Monster();
   var player = Player();
 
+  Duration timeDelay = const Duration(milliseconds: 3000);
+
+  void monsterattack() {
+    setState(() {
+      player.hp = player.hp - monster.attack;
+    });
+  }
+
   @override
   Widget build(BuildContext context) => Scaffold(
-      body: Center(
-          child: Column(
+          body: Center(
+              child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: <Widget>[
+          Text(monster.name),
+          Text('Monster Lv: ${(monster.lv).toString()}'),
+          Text('Monster HP: ${(monster.hp).round().toDouble()}'),
+          Text(player.name),
+          Text('Player Lv: ${(player.lv).toString()}'),
+          Text('Player HP: ${(player.hp).round().toDouble()}'),
+          Row(
             mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              Text(monster.name),
-              Text('Monster HP: ${monster.hp}'),
-              Text(player.name),
-              Text('Player HP: ${player.hp}'),
-              Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      setState(() {
-                        monster.hp -= 10;
-                        player.hp = player.hp - monster.attack;
-                      });
-                    },
-                    child: const Text('Attack'),
-                  ),
-                  const SizedBox(width: 10),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: const Text('Escape'),
-                  ),
-                ],
-              )
+            children: [
+              ElevatedButton(
+                onPressed: () {
+                  if (monster.hp <= 0) {
+                    setState(() {
+                      monster = Monster();
+                    });
+                  } else {
+                    setState(() {
+                      monster.hp -= player.attack;
+                    });
+                    Timer(timeDelay, monsterattack);
+                  }
+                },
+                child: const Text('Attack'),
+              ),
+              const SizedBox(width: 10),
+              ElevatedButton(
+                onPressed: () {},
+                child: const Text('Escape'),
+              ),
             ],
-          )));
+          )
+        ],
+      )));
 }
 
 class ProfileScreen extends StatelessWidget {
+  const ProfileScreen({super.key});
+
   @override
   Widget build(BuildContext context) {
     return const Center(
